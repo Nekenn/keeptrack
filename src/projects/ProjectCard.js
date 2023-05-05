07b67e5278971eef641b603d2ba57ca5c9571caf
Nekenn <sarobidinirina1997@gmail.com>
2023-05-05 18:31:03 +0300

    next developement

--- src/projects/ProjectCard.js
+++ src/projects/ProjectCard.js
@@ -7,14 +7,24 @@ function formatDescription(description) {
 
 function ProjectCard(props) {
     const { project } = props;
+    const handleclick = (projectBeingToUpdate) =>{
+       console.log(projectBeingToUpdate);
+    }
     return (
-                    <div className="card">
+                   <div className="card">
                         <img src={project.imageUrl} alt = {project.name} />
                         <section className = "section dark">
                             <strong>{project.name}</strong>
                             <p>{formatDescription(project.description)}</p>
                             <p>Budget: {project.budget.toLocaleString()}</p>
+			    <button className="bordered">
+						<span className="icon-edit"></span>
+	    		        edit
+
+	    		    </button>
+
                         </section>
+
                     </div>
     );
 }
